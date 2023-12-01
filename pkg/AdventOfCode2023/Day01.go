db27329f1cea9299c48e7e007268fa2d23fd111b
Radamés <r.alfaro8@icloud.com>
2023-12-01 02:24:42 -0400

    AOC2023 Day01 Part02 Solution…?
    Apparently, there's something odd with this. Let's check it out later.

--- pkg/AdventOfCode2023/Day01.go
+++ pkg/AdventOfCode2023/Day01.go
@@ -49,18 +49,7 @@ func (s Day01) part01() string {
 	// Use regex to find digit in last position
 	last := regexp.MustCompile("(\\d)(?:[a-zA-Z]*)$")
 
-	// Grab entries from original array
-	var sum int = 0
-
-	for _, line := range s.data {
-		d0 := first.FindStringSubmatch(line)
-		d1 := last.FindStringSubmatch(line)
-
-		number, _ := strconv.Atoi(d0[1] + d1[1])
-
-		// Sum array
-		sum += number
-	}
+	sum := findSubstrings(s.data, first, last)
 
 	return fmt.Sprintf("%d", sum)
 }
@@ -68,6 +57,80 @@ func (s Day01) part01() string {
 
 func (s Day01) part02() string {
 
-	return fmt.Sprintf("%d", -1)
+	digits := map[string]int{
+		"one": 1,
+		"two": 2,
+		"three": 3,
+		"four": 4,
+		"five": 5,
+		"six": 6,
+		"seven": 7,
+		"eight": 8,
+		"nine": 9,
+		"1": 1,
+		"2": 2,
+		"3": 3,
+		"4": 4,
+		"5": 5,
+		"6": 6,
+		"7": 7,
+		"8": 8,
+		"9": 9,
+	}
+
+	words := ""
+
+	for key, _ := range digits {
+		words += "|" + key
+	}
+
+	rewords := regexp.MustCompile(words[1:])
+
+	// Grab entries from original array
+	var sum int = 0
+
+	for _, line := range s.data {
+		fmt.Println("---", line)
+
+		idx := rewords.FindAllStringSubmatchIndex(line, -1)
+
+		fmt.Println(idx)
+
+		for _, match := range idx {
+			fmt.Println(line[match[0]:match[1]])
+			fmt.Println("------")
+		}
+
+		d0 := digits[line[idx[0][0]:idx[0][1]]]
+		d1 := digits[line[idx[len(idx)-1][0]:idx[len(idx)-1][1]]]
+
+		// Sum array
+		number := d0 * 10 + d1
+
+		fmt.Println(">> ", number)
+		sum += number
+	}
+
+	return fmt.Sprintf("%d", sum)
+}
+
+
+func findSubstrings(array []string, re0 *regexp.Regexp, re1 *regexp.Regexp) int {
+	// Grab entries from original array
+	var sum int = 0
+
+	for _, line := range array {
+		d0 := re0.FindStringSubmatch(line)
+		d1 := re1.FindStringSubmatch(line)
+
+		fmt.Println(d0)
+
+		number, _ := strconv.Atoi(d0[1] + d1[1])
+
+		// Sum array
+		sum += number
+	}
+
+	return sum
 }
 
