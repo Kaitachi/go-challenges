6a834e730012dd29df00fd22262b82b5c94637db
Radamés <r.alfaro8@icloud.com>
2023-12-10 20:10:16 -0400

    AOC2023 Day10 Part02 Solution
    My code works… I have no idea why…

--- pkg/AdventOfCode2023/Day10.go
+++ pkg/AdventOfCode2023/Day10.go
@@ -12,6 +12,9 @@ import (
 type Day10 struct {
 	start *cell
 	grid []map[int]*cell
+
+	rows int
+	cols int
 }
 
 
@@ -19,6 +22,8 @@ type cell struct {
 	name		string
 	row			int
 	col			int
+	isMainLoop	bool
+	isFilled	bool
 	directions	map[string]*cell
 }
 
@@ -26,10 +31,12 @@ type cell struct {
 // 1. Assemble - How should we transform the data from our input files?
 func (s *Day10) Assemble(tc *lib.TestCase) {
 
+	s.rows = 0
+	s.cols = 0
 	s.start = nil
 	s.grid = make([]map[int]*cell, 0)
 
-	re_pipe := regexp.MustCompile(`[|\-LJ7FS]`)
+	re_pipe := regexp.MustCompile(`[|\-LJ7FS.]`)
 
 	for row, line := range strings.Split(tc.Input, "\n") {
 		s.grid = append(s.grid, make(map[int]*cell, 0))
@@ -69,17 +76,103 @@ func (s *Day10) Activate(tc *lib.TestCase) {
 
 func (s Day10) part01() string {
 
-	// fmt.Printf("%+v\n", s.start)
+	steps := s.drawLoop()
+
+	return fmt.Sprintf("%d", steps)
+}
+
+
+func (s Day10) part02() string {
+
+	s.drawLoop()
+
+
+	// Let's replace our starting position
+	_, okNorth := s.start.directions["N"]
+	_, okEast := s.start.directions["E"]
+	_, okSouth := s.start.directions["S"]
+	_, okWest := s.start.directions["W"]
+	links := fmt.Sprintf("%5t-%5t-%5t-%5t", okNorth, okEast, okSouth, okWest)
+
+	switch links {
+	case "false- true- true-false": s.start.name = "F"
+	case " true- true-false-false": s.start.name = "L"
+	case "false-false- true- true": s.start.name = "7"
+	case " true-false-false- true": s.start.name = "J"
+	case " true-false- true-false": s.start.name = "|"
+	case "false- true-false- true": s.start.name = "-"
+	}
+
+	inner := make([]*cell, 0)
+	inLoopCol := make([]bool, s.cols) // Scanning from top to bottom, could we say this cell could be within our loop?
+	loopColCorner := make([]string, s.cols)
+
+	for row, _ := range s.grid {
+		inLoopRow := false
+
+		for col := 0; col < s.cols; col++ {
+			cell := s.grid[row][col]
+
+			if cell.isMainLoop {
+				// Are we entering/exiting the loop?
+				switch cell.name {
+				case "|":
+					inLoopRow = !inLoopRow
+
+				case "-", "7", "F":
+					inLoopCol[col] = !inLoopCol[col]
+					switch cell.name {
+					case "7", "F":
+						loopColCorner[col] = cell.name
+					}
+
+				case "L":
+					inLoopRow = !inLoopRow
+
+					if loopColCorner[col] == "F" {
+						inLoopCol[col] = !inLoopCol[col]
+						loopColCorner[col] = ""
+					}
+
+				case "J":
+					inLoopRow = !inLoopRow
+
+					if loopColCorner[col] == "7" {
+						inLoopCol[col] = !inLoopCol[col]
+						loopColCorner[col] = ""
+					}
+
+				}
+			} else if inLoopCol[col] && inLoopRow {
+				// We're not touching the loop. This cell would then be inside of our loop.
+				cell.isFilled = true
+				inner = append(inner, cell)
+			}
+
+			if row == 4 && col == 7 {
+				fmt.Printf(">>>> (%d, %d): inLoopRow: %t; inLoopCol: %t\n", row, col, inLoopRow, inLoopCol[col])
+			}
+		}
+	}
+
+	fmt.Printf("%+v\n", s.start)
 	// for i := 0; i < len(s.grid); i++ {
-	// 	fmt.Printf("%v\n", s.grid[i])
-	// 
-	// 	for j, pipe := range s.grid[i] {
-	// 		fmt.Printf("[%d]: %+v\n", j, pipe)
-	// 	}
+	// 	fmt.Printf("[%d]: %v\n", i, s.grid[i])
 	//
-	// 	fmt.Println()
+	// 	// for j, pipe := range s.grid[i] {
+	// 	// 	fmt.Printf("[%d]: %+v\n", j, pipe)
+	// 	// }
 	// }
 
+	fmt.Printf("%v\n", inner)
+
+	s.printMainLoop()
+
+	return fmt.Sprintf("%d", len(inner))
+}
+
+
+func (s *Day10) drawLoop() int {
 	// Let's make two pointers, one travels left and the other travels right
 	var left, right *cell
 	var prev_left, prev_right *cell
@@ -100,29 +193,21 @@ func (s Day10) part01() string {
 		prev_right, right = right.next(prev_right)
 	}
 
-	fmt.Printf("> steps: %d\n", steps)
-	fmt.Printf("> left: %+v\n", left)
-	fmt.Printf("> right: %+v\n", right)
+	// Mark first and last nodes that are visited as part of the loop
+	s.start.isMainLoop = true
+	left.isMainLoop = true
 
-	return fmt.Sprintf("%d", steps)
-}
-
-
-func (s Day10) part02() string {
-
-	return fmt.Sprintf("%d", -1)
+	return steps
 }
 
 
 func (s *Day10) linkPipes() {
-	rows := 0
-	cols := 0
 
 	for _, line := range s.grid {
-		rows++
+		s.rows++
 
 		for col, _ := range line {
-			cols = int(math.Max(float64(cols), float64(col)))
+			s.cols = int(math.Max(float64(s.cols), float64(col)))
 		}
 	}
 
@@ -140,7 +225,7 @@ func (s *Day10) linkPipes() {
 			}
 
 			// Looking East, can we connect to our neighbour?
-			if col+1 <= cols && (pipe.hasPipeFacing("E") || isStart) {
+			if col+1 <= s.cols && (pipe.hasPipeFacing("E") || isStart) {
 				east := s.grid[row][col+1]
 
 				if east != nil && east.hasPipeFacing("W") {
@@ -149,7 +234,7 @@ func (s *Day10) linkPipes() {
 			}
 
 			// Looking South, can we connect to our neighbour?
-			if row+1 < rows && (pipe.hasPipeFacing("S") || isStart) {
+			if row+1 < s.rows && (pipe.hasPipeFacing("S") || isStart) {
 				south := s.grid[row+1][col]
 
 				if south != nil && south.hasPipeFacing("N") {
@@ -167,6 +252,11 @@ func (s *Day10) linkPipes() {
 			}
 		}
 	}
+}
+
+
+func (c cell) isPipe() bool {
+	return c.name != "."
 }
 
 
@@ -197,6 +287,8 @@ func (c cell) hasPipeFacing(d string) bool {
 
 
 func (c *cell) next(prev *cell) (*cell, *cell) {
+	c.isMainLoop = true
+
 	for _, neighbor := range c.directions {
 		if neighbor != prev {
 			return c, neighbor
@@ -204,5 +296,26 @@ func (c *cell) next(prev *cell) (*cell, *cell) {
 	}
 
 	return nil, nil
+}
+
+
+func (s *Day10) printMainLoop() {
+
+	fmt.Printf("%d rows, %d cols\n", s.rows, s.cols)
+	for row := 0; row < s.rows; row++ {
+		fmt.Printf("[%d]: ", row)
+	
+		for col := 0; col < s.cols; col++ {
+			cell := s.grid[row][col]
+
+			switch {
+			case cell.isMainLoop:	fmt.Printf("%s", cell.name)
+			case cell.isFilled:		fmt.Printf("I")
+			default:				fmt.Printf(".")
+			}
+		}
+
+		fmt.Printf("\n")
+	}
 }
 
